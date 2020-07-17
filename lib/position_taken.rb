cfe5c4ae77a6a3b90066e4f3612852f09b76bbe0
Mary Dick <maryclairedick@gmail.com>
2020-07-17 18:50:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@ def position_taken?(board, index)
   
   if board[index] != " " || "" || nil
     return false
-    
-  elsif board[index] == "X" || "O"
-    return !true
+  end
+  if board[index] == "X" || "O"
+    return true
   end
     
 end
